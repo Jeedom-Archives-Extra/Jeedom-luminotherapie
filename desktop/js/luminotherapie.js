a49ae528f81c4ae0c8de1a18a2c91366c3857bcb
Halbout Michael <michaelhalbout@gmail.com>
2017-11-03 14:23:41 +0100

    Update luminotherapie.js

--- desktop/js/luminotherapie.js
+++ desktop/js/luminotherapie.js
@@ -30,3 +30,103 @@ $("body").on('click', ".listCmdAction", function() {
 		el.value(result.human);
 	});
 });
+
+$("body").on('change', ".eqLogicAttr[data-l2key=DawnSimulatorEngineType]", function() {
+	var Series = [{
+		step: true,
+		name: '{{Simulation}}',
+		data: 0,
+		type: 'line',
+		tooltip: {
+			valueDecimals: 2
+		},
+	}];
+	drawSimpleGraph($('#GraphSim'), Series);
+});
+function drawSimpleGraph(_el, _serie) {
+    var legend = {
+        enabled: true,
+        borderColor: 'black',
+        borderWidth: 2,
+        shadow: true
+    };
+
+    new Highcharts.StockChart({
+        chart: {
+            zoomType: 'x',
+            renderTo: _el,
+            height: 350,
+            spacingTop: 0,
+            spacingLeft: 0,
+            spacingRight: 0,
+            spacingBottom: 0
+        },
+        credits: {
+            text: 'Copyright Jeedom',
+            href: 'http://jeedom.fr',
+        },
+        navigator: {
+            enabled: false
+        },
+        rangeSelector: {
+            buttons: [{
+                    type: 'minute',
+                    count: 30,
+                    text: '30m'
+                }, {
+                    type: 'hour',
+                    count: 1,
+                    text: 'H'
+                }, {
+                    type: 'day',
+                    count: 1,
+                    text: 'J'
+                }, {
+                    type: 'week',
+                    count: 1,
+                    text: 'S'
+                }, {
+                    type: 'month',
+                    count: 1,
+                    text: 'M'
+                }, {
+                    type: 'year',
+                    count: 1,
+                    text: 'A'
+                }, {
+                    type: 'all',
+                    count: 1,
+                    text: 'Tous'
+                }],
+            selected: 6,
+            inputEnabled: false
+        },
+        legend: legend,
+        tooltip: {
+            pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b><br/>',
+            valueDecimals: 2,
+        },
+        yAxis: {
+            format: '{value}',
+            showEmpty: false,
+            showLastLabel: true,
+            min: 0,
+            labels: {
+                align: 'right',
+                x: -5
+            }
+        },
+        scrollbar: {
+            barBackgroundColor: 'gray',
+            barBorderRadius: 7,
+            barBorderWidth: 0,
+            buttonBackgroundColor: 'gray',
+            buttonBorderWidth: 0,
+            buttonBorderRadius: 7,
+            trackBackgroundColor: 'none', trackBorderWidth: 1,
+            trackBorderRadius: 8,
+            trackBorderColor: '#CCC'
+        },
+        series: _serie
+    });
+}
