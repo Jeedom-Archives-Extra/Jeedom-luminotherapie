e3467a17a33e8c80eb708293389afb71dc16a406
Halbout Michael <michaelhalbout@gmail.com>
2017-11-07 08:38:04 +0100

    Update luminotherapie.ajax.php

--- core/ajax/luminotherapie.ajax.php
+++ core/ajax/luminotherapie.ajax.php
@@ -1,26 +1,26 @@
 <?php
-  try {
-    require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
-    include_file('core', 'authentification', 'php');
-    if (!isConnect('admin')) {
-      throw new Exception(__('401 - Accès non autorisé', __FILE__));
-    }
-    if (init('action') == 'SimulaitonPoint') {		
-    $point=null;
-      for($time=init('DawnSimulatorEngineStartValue');$time<=init('DawnSimulatorEngineEndValue');$time++){
-					  $point[] = ceil(luminotherapie::dawnSimulatorEngine(
-						init('DawnSimulatorEngineType'),
-						$time,
-						init('DawnSimulatorEngineStartValue'), 
-						init('DawnSimulatorEngineEndValue'), 
-						init('DawnSimulatorEngineDuration')
-					));
-				}
-      ajax::success(json_encode($point));		
-    }
-    throw new Exception(__('Aucune methode correspondante à : ', __FILE__) . init('action'));
-    /*     * *********Catch exeption*************** */
-  } catch (Exception $e) {
-   ajax::error(displayExeption($e), $e->getCode());
-  }
+	try {
+		require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
+		include_file('core', 'authentification', 'php');
+		if (!isConnect('admin')) {
+			throw new Exception(__('401 - Accès non autorisé', __FILE__));
+		}
+		if (init('action') == 'SimulaitonPoint') {		
+			$point=null;
+			for($time=init('DawnSimulatorEngineStartValue');$time<=init('DawnSimulatorEngineEndValue');$time++){
+				$point[] = array(ceil(luminotherapie::dawnSimulatorEngine(
+					init('DawnSimulatorEngineType'),
+					$time,
+					init('DawnSimulatorEngineStartValue'), 
+					init('DawnSimulatorEngineEndValue'), 
+					init('DawnSimulatorEngineDuration')
+				)),$time);
+			}
+			ajax::success(json_encode($point));		
+		}
+		throw new Exception(__('Aucune methode correspondante à : ', __FILE__) . init('action'));
+		/*     * *********Catch exeption*************** */
+	} catch (Exception $e) {
+		ajax::error(displayExeption($e), $e->getCode());
+	}
 ?>
