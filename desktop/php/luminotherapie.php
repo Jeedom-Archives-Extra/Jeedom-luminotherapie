606a283ff0315ccf37a25244f08d4865fbfcb3d9
Halbout Michael <michaelhalbout@gmail.com>
2017-11-30 08:29:22 +0100

    Update luminotherapie.php

--- desktop/php/luminotherapie.php
+++ desktop/php/luminotherapie.php
@@ -1,53 +1,84 @@
 <?php
 if (!isConnect('admin')) {
-throw new Exception('{{401 - Accès non autorisé}}');
+    throw new Exception('{{401 - Accès non autorisé}}');
 }
 sendVarToJS('eqType', 'luminotherapie');
 $eqLogics = eqLogic::byType('luminotherapie');
+$ambiances=ls($directory, '*');
 ?>
 <div class="row row-overflow">
-	<div class="col-lg-2">
-		<div class="bs-sidebar">
-			<ul id="ul_eqLogic" class="nav nav-list bs-sidenav">
-				<a class="btn btn-default eqLogicAction" style="width : 50%;margin-top : 5px;margin-bottom: 5px;" data-action="add"><i class="fa fa-plus-circle"></i> {{Ajouter}}</a>
-				<li class="filter" style="margin-bottom: 5px;"><input class="filter form-control input-sm" placeholder="{{Rechercher}}" style="width: 100%"/></li>
-				<?php
-					foreach ($eqLogics as $eqLogic) 
-						echo '<li class="cursor li_eqLogic" data-eqLogic_id="' . $eqLogic->getId() . '"><a>' . $eqLogic->getHumanName(true) . '</a></li>';
-				?>
-			</ul>
-		</div>
-	</div>
+    <div class="col-lg-2 col-md-3 col-sm-4">
+        <div class="bs-sidebar">
+                <a class="btn btn-default ambianceAction" style="width : 100%;margin-top : 5px;margin-bottom: 5px;" data-action="add">
+					<i class="fa fa-plus-circle"></i> {{Ajouter une ambiance}}
+				</a>
+                <a class="btn btn-default eqLogicAction" style="width : 100%;margin-top : 5px;margin-bottom: 5px;" data-action="add">
+					<i class="fa fa-plus-circle"></i> {{Ajouter une simulation}}
+				</a>
+                <li class="filter" style="margin-bottom: 5px;">
+					<input class="filter form-control input-sm" placeholder="{{Rechercher}}" style="width: 100%"/>
+				</li>
+                <?php
+                foreach (eqLogic::byType('luminotherapie') as $eqLogic) {
+					echo '<li class="cursor li_eqLogic" data-eqLogic_id="' . $eqLogic->getId() . '"><a>' . $eqLogic->getHumanName(true) . '</a></li>';
+                }
+                ?>
+            </ul>
+        </div>
+    </div>
 	<div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
-		<legend>{{Mes luminotherapies}}</legend>
+		<legend>{{Gestion}}</legend>
 		<div class="eqLogicThumbnailContainer">
-			<div class="cursor eqLogicAction" data-action="add" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
+			<div class="cursor eqLogicAction" data-action="add" style="background-color : #ffffff; height : 140px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
 				<center>
-					<i class="fa fa-plus-circle" style="font-size : 7em;color:#406E88;"></i>
+					<i class="fa fa-plus-circle" style="font-size : 5em;color:#406E88;"></i>
 				</center>
-				<span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;;color:#406E88"><center>Ajouter</center></span>
+				<span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#406E88"><center>{{Ajouter simulation}}</center></span>
 			</div>
+			<div class="cursor ambianceAction" data-action="add" style="background-color : #ffffff; height : 140px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
+				<center>
+					<i class="fa fa-plus-circle" style="font-size : 5em;color:#406E88;"></i>
+				</center>
+				<span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#406E88"><center>{{Ajouter ambiance}}</center></span>
+			</div>
+			<div class="cursor eqLogicAction" data-action="gotoPluginConf" style="height: 120px; margin-bottom: 10px; padding: 5px; border-radius: 2px; width: 160px; margin-left: 10px; position: absolute; left: 170px; top: 0px; background-color: rgb(255, 255, 255);">
+				<center>
+			      		<i class="fa fa-wrench" style="font-size : 5em;color:#767676;"></i>
+			    	</center>
+			    	<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#767676"><center>Configuration</center></span>
+			</div>
+		</div>
+        <legend>{{Mes ambiances}}</legend>
+		<div class="eqLogicThumbnailContainer">
 			<?php
+			if (count($ambiances) == 0) {
+				echo "<br/><br/><br/><center><span style='color:#767676;font-size:1.2em;font-weight: bold;'>{{Vous n'avez pas encore d'ambiance de configurer, cliquez sur Ajouter pour commencer}}</span></center>";
+			} else {
+				foreach ($ambiances as $key => $ambiance) {
+					echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $key . '" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >';
+					echo '<center><img src="plugins/luminotherapie/plugin_info/luminotherapie_icon.png" height="105" width="95" /></center>';
+					echo '<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;"><center>' . $ambiance['name'] . '</center></span>';
+					echo '</div>';
+				}
+			} 
+			?>
+		</div>
+        <legend>{{Mes simulations}}</legend>
+		<div class="eqLogicThumbnailContainer">
+			<?php
+			if (count($eqLogics) == 0) {
+				echo "<br/><br/><br/><center><span style='color:#767676;font-size:1.2em;font-weight: bold;'>{{Vous n'avez pas encore de simulation, cliquez sur Ajouter pour commencer}}</span></center>";
+			} else {
 				foreach ($eqLogics as $eqLogic) {
-					$opacity = '';
-					if ($eqLogic->getIsEnable() != 1) {
-						$opacity = '
-						-webkit-filter: grayscale(100%);
-						-moz-filter: grayscale(100);
-						-o-filter: grayscale(100%);
-						-ms-filter: grayscale(100%);
-						filter: grayscale(100%); opacity: 0.35;';
-					}
-					echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;' . $opacity . '" >';
-					echo "<center>";
-					echo '<img src="plugins/luminotherapie/plugin_info/luminotherapie_icon.png" height="105" width="95" />';
-					echo "</center>";
+					echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >';
+					echo '<center><img src="plugins/luminotherapie/plugin_info/luminotherapie_icon.png" height="105" width="95" /></center>';
 					echo '<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;"><center>' . $eqLogic->getHumanName(true, true) . '</center></span>';
 					echo '</div>';
 				}
+			} 
 			?>
 		</div>
-	</div>  
+    </div>
 	<div class="col-lg-10 col-md-9 col-sm-8 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
 		<a class="btn btn-success eqLogicAction pull-right" data-action="save"><i class="fa fa-check-circle"></i> Sauvegarder</a>
 		<a class="btn btn-danger eqLogicAction pull-right" data-action="remove"><i class="fa fa-minus-circle"></i> Supprimer</a>
@@ -67,14 +98,10 @@ $eqLogics = eqLogic::byType('luminotherapie');
 				<a href="#commandtab" aria-controls="profile" role="tab" data-toggle="tab" aria-expanded="false">
 					<i class="fa fa-list-alt"></i> Commandes</a>
 			</li>
-			<li role="presentation" class="">
-				<a href="#signaltab" aria-controls="profile" role="tab" data-toggle="tab" aria-expanded="false">
-					<i class="fa fa-list-alt"></i> Génération de l'ambiance</a>
-			</li>
 		</ul>
 		<div class="tab-content" style="height:calc(100% - 50px);overflow:auto;overflow-x: hidden;">
 			<div role="tabpanel" class="tab-pane active" id="eqlogictab">
-				<div class="col-lg-6">
+								<div class="col-lg-6">
 					<legend>Général</legend>
 					<form class="form-horizontal">
 						<fieldset>
@@ -225,16 +252,69 @@ $eqLogics = eqLogic::byType('luminotherapie');
 			</div>
 			<div role="tabpanel" class="tab-pane" id="commandtab">	
 				<table id="table_cmd" class="table table-bordered table-condensed">
-				    <thead>
+					<thead>
 					<tr>
-					    <th>Nom</th>
-					    <th>Paramètre</th>
+						<th>Nom</th>
+						<th>Paramètre</th>
 					</tr>
-				    </thead>
-				    <tbody></tbody>
+					</thead>
+					<tbody></tbody>
 				</table>
 			</div>	
-			<div role="tabpanel" class="tab-pane" id="signaltab">	
+		</div>
+	</div>
+	<div class="col-lg-10 col-md-9 col-sm-8 ambiance" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
+		<a class="btn btn-success ambianceAction pull-right" data-action="save"><i class="fa fa-check-circle"></i> Sauvegarder</a>
+		<a class="btn btn-danger ambianceAction pull-right" data-action="remove"><i class="fa fa-minus-circle"></i> Supprimer</a>
+		<a class="btn btn-default ambianceAction pull-right" data-action="copy"><i class="fa fa-copy"></i>{{Dupliquer}}</a>
+		<ul class="nav nav-tabs" role="tablist">
+			<li role="presentation">
+				<a href="#" class="ambianceAction" aria-controls="home" role="tab" data-toggle="tab" data-action="returnToThumbnailDisplay">
+					<i class="fa fa-arrow-circle-left"></i>
+				</a>
+			</li>
+			<li role="presentation" class="active">
+				<a href="#luminotab" aria-controls="home" role="tab" data-toggle="tab" aria-expanded="true">
+					<i class="fa fa-tachometer"></i> Luminosité</a>
+			</li>
+			<li role="presentation" class="">
+				<a href="#colortab" aria-controls="profile" role="tab" data-toggle="tab" aria-expanded="false">
+					<i class="fa fa-list-alt"></i> Couleur</a>
+			</li>
+		</ul>
+		<div class="tab-content" style="height:calc(100% - 50px);overflow:auto;overflow-x: hidden;">
+			<div role="tabpanel" class="tab-pane" id="luminotab">	
+				<form class="form-horizontal">
+					<fieldset>
+						<legend>{{Creation de votre simulation :}}
+							<sup>
+								<i class="fa fa-question-circle tooltips" title="Créer par ajout de séquence votre ambiance lumineuse"></i>
+							</sup>
+							<a class="btn btn-success btn-xs sequenceAttr" data-action="add" style="margin-left: 5px;">
+								<i class="fa fa-plus-circle"></i>
+								{{Ajouter une séquence}}
+							</a>
+						</legend>
+					</fieldset>
+				</form>		
+				<div>	
+					<legend>Votre simulation</legend>
+					<div id='SeqGraph'></div>
+				</div>	
+				<div>	
+					<table id="SeqList" class="table table-bordered table-condensed">
+						<thead>
+							<tr>
+								<th></th>
+								<th>{{Type}}</th>
+								<th>{{Paramètre}}</th>
+							</tr>
+						</thead>
+						<tbody></tbody>
+					</table>
+				</div>	
+			</div>
+			<div role="tabpanel" class="tab-pane" id="colortab">	
 				<form class="form-horizontal">
 					<fieldset>
 						<legend>{{Creation de votre simulation :}}
@@ -269,5 +349,7 @@ $eqLogics = eqLogic::byType('luminotherapie');
 	</div>
 </div>
 
-<?php include_file('desktop', 'luminotherapie', 'js', 'luminotherapie'); ?>
-<?php include_file('core', 'plugin.template', 'js'); ?>
+<?php 
+include_file('desktop', 'luminotherapie', 'js', 'luminotherapie');
+include_file('core', 'plugin.template', 'js'); 
+?>
