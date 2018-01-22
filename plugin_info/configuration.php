6387f63f7af4ffceba6abc82693113c1aaab4949
Halbout Michael <michaelhalbout@gmail.com>
2018-01-22 11:33:26 +0100

    Update configuration.php

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -1,21 +1,3 @@
-Skip to content
-This repository
-Search
-Pull requests
-Issues
-Marketplace
-Explore
- @mika-nt28
- Sign out
- Unwatch 5
-  Star 5  Fork 5 mika-nt28/Jeedom-Eibd
- Code  Issues 1  Pull requests 0  Projects 0  Wiki  Insights  Settings
-Branch: beta Find file Copy pathJeedom-Eibd/plugin_info/configuration.php
-6a008c9  on 4 Oct 2017
-@mika-nt28 mika-nt28 Update configuration.php
-2 contributors @mika-nt28 @bbreton09
-RawBlameHistory     
-134 lines (132 sloc)  4.06 KB
 <?php
 require_once dirname(__FILE__) . '/../../../core/php/core.inc.php';
 include_file('core', 'authentification', 'php');
